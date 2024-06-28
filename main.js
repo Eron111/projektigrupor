2d928138e229204a4e639745d38705bab6e91f53
e-NIDDI <149226186+e-NIDDI@users.noreply.github.com>
2024-06-28 12:07:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -60,3 +60,25 @@ $(document).ready(function(){
         })
     }
   });
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const tosButton = document.getElementById('tos-button');
+    const modal = document.getElementById('tos-modal');
+    const closeButton = document.getElementsByClassName('close2')[0];
+
+    tosButton.onclick = function() {
+        modal.style.display = 'block';
+    }
+
+    closeButton.onclick = function() {
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+});
