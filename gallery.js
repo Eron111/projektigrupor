bbaef245aeed85655e15bc8fa998cfcf6b1207c0
e-NIDDI <enidbajrami1@gmail.com>
2024-06-28 12:12:26 +0200

    qwf

--- gallery.js
+++ gallery.js
@@ -33,3 +33,27 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const tosButton = document.getElementById('tos-button');
+    const modal = document.getElementById('tos-modal');
+    const closeButton = document.getElementsByClassName('close2')[0];
+
+    tosButton.onclick = function() {
+        modal.style.display = 'block';
+    }
+
+    closeButton.onclick = function() {
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+});
+
